c4dedce0f8544f1d3b66418aa8ab59b670a8b6ee
luizhenriquea2002-cell <luizhenriquea2002@gmail.com>
2026-04-01 16:09:15 -0300

    Implement user saldo management in server.js

--- luxuriaplay/server.js
+++ luxuriaplay/server.js
@@ -1,1 +1,34 @@
+let usersData = {};
 
+app.post("/play", auth, (req, res) => {
+  const email = req.userEmail;
+
+  if (!usersData[email]) {
+    usersData[email] = { saldo: 1000 };
+  }
+
+  const chance = Math.random();
+  let win = false;
+  let premio = 0;
+
+  if (chance > 0.9) {
+    premio = 300;
+    win = true;
+  } else {
+    premio = -50;
+  }
+
+  usersData[email].saldo += premio;
+
+  res.json({ win, saldo: usersData[email].saldo });
+});
+
+app.get("/saldo", auth, (req, res) => {
+  const email = req.userEmail;
+
+  if (!usersData[email]) {
+    usersData[email] = { saldo: 1000 };
+  }
+
+  res.json({ saldo: usersData[email].saldo });
+});

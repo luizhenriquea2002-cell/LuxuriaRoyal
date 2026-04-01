cecb4427be926969f407c11c316be8158586d533
luizhenriquea2002-cell <luizhenriquea2002@gmail.com>
2026-04-01 17:57:35 -0300

    Update game.js

--- game.js
+++ game.js
@@ -20,3 +20,37 @@ function girarSlot() {
     }, 100);
   });
 }
+let saldo = 5000;
+
+function atualizarSaldo() {
+  document.getElementById("dados").innerHTML = "💰 Saldo: " + saldo;
+}
+
+function girarSlot() {
+  if (saldo < 100) {
+    alert("Saldo insuficiente");
+    return;
+  }
+
+  saldo -= 100;
+
+  const simbolos = ["💎","🔥","💰","🍀","👑"];
+  let resultado = [];
+
+  for (let i = 0; i < 3; i++) {
+    resultado.push(simbolos[Math.floor(Math.random() * simbolos.length)]);
+  }
+
+  if (resultado[0] === resultado[1] && resultado[1] === resultado[2]) {
+    saldo += 1000;
+    alert("🎉 JACKPOT!");
+  }
+
+  document.getElementById("r1").innerText = resultado[0];
+  document.getElementById("r2").innerText = resultado[1];
+  document.getElementById("r3").innerText = resultado[2];
+
+  atualizarSaldo();
+}
+
+atualizarSaldo();
